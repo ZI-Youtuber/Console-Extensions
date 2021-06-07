32660b28868b7de22562b9c21bc88eeddc0014b7
ZI Coder <82158378+ZI-Youtuber@users.noreply.github.com>
2021-06-07 05:26:04 +0100

    Create OpenScratchProjectInLargeSize.js

---
+++ Scratch Console Extensions/OpenScratchProjectInLargeSize.js
@@ -0,0 +1,4 @@
+// This Console Extension will redirect you to a scratch project in Large Size when you write its id number 
+// Credit to ZI-Youtuber
+
+javascript:var embedid = prompt("What Scratch Project Do You want in Large Size?"); window.location.href="https://scratch.mit.edu/projects/"+embedid+"/embed";
