a00fd89aa878eeeb39d147c4c324a77407af9dc5
ZI Coder <82158378+ZI-Youtuber@users.noreply.github.com>
2021-06-07 05:25:16 +0100

    Create RedirectToScratchProject.js

---
+++ Scratch Console Extensions/RedirectToScratchProject.js
@@ -0,0 +1,4 @@
+// This Console Extension will redirect you to a scratch project when you write its id number 
+// Credit to ZI-Youtuber
+
+javascript:var id = prompt("What Scratch Project Do You want to go to?"); window.location.href="https://scratch.mit.edu/projects/"+id+"/";
