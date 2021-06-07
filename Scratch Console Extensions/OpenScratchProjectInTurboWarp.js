025342616d0e0f170c509ab5eaf6e73359c395af
ZI Coder <82158378+ZI-Youtuber@users.noreply.github.com>
2021-06-07 05:25:44 +0100

    Create OpenScratchProjectInTurboWarp.js

---
+++ Scratch Console Extensions/OpenScratchProjectInTurboWarp.js
@@ -0,0 +1,4 @@
+// This Console Extension will take you to the turbowarp version of a scratch project onces you write the scratch project's id
+// Credit to ZI-Youtuber
+
+javascript:var id = prompt("What Scratch Project Would you like to open in Turbowarp?"); window.location.href="https://turbowarp.org/"+id+"/";
