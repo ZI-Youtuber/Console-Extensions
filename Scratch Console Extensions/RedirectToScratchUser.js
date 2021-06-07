8d1666b6a14b79da0dbac6a2402e5ae444e08b59
ZI Coder <82158378+ZI-Youtuber@users.noreply.github.com>
2021-06-07 05:24:40 +0100

    Create RedirectToScratchUser.js

---
+++ Scratch Console Extensions/RedirectToScratchUser.js
@@ -0,0 +1,4 @@
+// This Console Extension will redirect you to a scratch user when you write their username
+// Credit to ZI-Youtuber
+
+javascript:var user = prompt("What Scratch User Do You want to go to?"); window.location.href="https://scratch.mit.edu/"+user+"/";
