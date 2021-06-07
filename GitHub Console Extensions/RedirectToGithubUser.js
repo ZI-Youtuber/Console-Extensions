d8a975137d52f024764f29766041473109ecbacf
ZI Coder <82158378+ZI-Youtuber@users.noreply.github.com>
2021-06-07 05:29:13 +0100

    Create RedirectToGithubUser.js

---
+++ GitHub Console Extensions/RedirectToGithubUser.js
@@ -0,0 +1,5 @@
+// This code will take you to a Github User
+// Write the username of the github user that you want to go to
+// Credit to ZI-Youtuber
+
+javascript:var user = prompt("What Github User Do You want to go to?"); window.location.href="https://github.com/"+user+"/";
